18046522757564d2839ea9a4fdd0d77416a16463
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-14 12:50:11 +0530

    Remove obsolete web stack from native Android project

--- capacitor.config.ts
+++
@@ -1,3 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-const config: CapacitorConfig = { appId: 'com.gokul.instaclone', appName: 'InstaClone', webDir: 'dist', bundledWebRuntime: false };
-export default config;

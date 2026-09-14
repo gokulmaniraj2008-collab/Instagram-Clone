7ab135abed192c43cbb662e4c4205e848e15783b
GOKUL M <gokulmaniraj2008@gmail.com>
2026-09-14 12:16:38 +0530

    feat: add Android app configuration

---
+++ capacitor.config.ts
@@ -0,0 +1,3 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+const config: CapacitorConfig = { appId: 'com.gokul.instaclone', appName: 'InstaClone', webDir: 'dist', bundledWebRuntime: false };
+export default config;
